9944c3b307d4b9b5e8054e0a5278ccebb7ec89ca
Limiel104 <a.myjak1995@gmail.com>
2024-03-17 13:34:18 +0100

    Home screen composables

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -3,13 +3,42 @@ package com.example.recipeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.recipeapp.ui.theme.RecipeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +51,189 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeContent()
+                }
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun HomeContent(
+
+) {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues),
+        ) {
+            HomeSearchBar()
+
+            HomeTopCategories()
+
+            Card(
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(8.dp)
+                ) {
+                    Text(
+                        text = "Recipes",
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 24.sp,
+                        modifier = Modifier
+                            .padding(bottom = 8.dp)
+                    )
+
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(2)
+                    ) {
+                        items(10) {
+                            RecipeItem()
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeSearchBar(
+) {
+    SearchBar(
+        query = "searching",
+        onQueryChange = {},
+        onSearch = {},
+        active = false,
+        onActiveChange = {},
+        trailingIcon = {
+            IconButton(
+                onClick = {}
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.FilterList,
+                    contentDescription = "Filter"
+                )
+            }
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp)
+            .padding(horizontal = 16.dp)
+    ) {
+
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    RecipeAppTheme {
-        Greeting("Android")
+fun HomeTopCategories(
+) {
+    Card(
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+        ) {
+            Text(
+                text = "Top Categories",
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 24.sp
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                TopCategoryItem()
+                TopCategoryItem()
+                TopCategoryItem()
+                TopCategoryItem()
+                TopCategoryItem()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TopCategoryItem(
+) {
+    Card(
+        shape = RoundedCornerShape(10.dp),
+        border = BorderStroke(1.dp,MaterialTheme.colorScheme.secondary)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row() {
+                Icon(
+                    imageVector = Icons.Filled.Image,
+                    contentDescription = null,
+                )
+
+                Icon(
+                    imageVector = Icons.Filled.Image,
+                    contentDescription = null
+                )
+            }
+
+            Row {
+                Icon(
+                    imageVector = Icons.Filled.Image,
+                    contentDescription = null
+                )
+
+                Icon(
+                    imageVector = Icons.Filled.Image,
+                    contentDescription = null
+                )
+            }
+
+            Text(text = "Category")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RecipeItem() {
+    Card(
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
+    ) {
+        AsyncImage(
+            model = ImageRequest
+                .Builder(LocalContext.current)
+                .data(R.drawable.ic_image)
+                .crossfade(true)
+                .placeholder(R.drawable.ic_image)
+                .build(),
+            contentDescription = "IMAGE",
+            fallback = painterResource(R.drawable.ic_image),
+            error = painterResource(R.drawable.ic_image),
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier
+                .clickable {}
+        )
     }
 }
