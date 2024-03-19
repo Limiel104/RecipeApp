239c1795489c34756ce27bc0c6bc66054912e318
Limiel104 <a.myjak1995@gmail.com>
2024-03-19 21:48:27 +0100

    Add recipe screen composables

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -20,9 +20,11 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.FilterList
 import androidx.compose.material.icons.filled.Image
+import androidx.compose.material.icons.outlined.PhotoCamera
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -31,10 +33,13 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -100,7 +105,9 @@ fun HomeContent(
                     )
 
                     LazyVerticalGrid(
-                        columns = GridCells.Fixed(2)
+                        columns = GridCells.Fixed(2),
+                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
                     ) {
                         items(10) {
                             RecipeItem()
@@ -184,7 +191,7 @@ fun TopCategoryItem(
 ) {
     Card(
         shape = RoundedCornerShape(10.dp),
-        border = BorderStroke(1.dp,MaterialTheme.colorScheme.secondary)
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.secondary)
     ) {
         Column(
             modifier = Modifier
@@ -224,25 +231,34 @@ fun TopCategoryItem(
 @Composable
 fun RecipeItem() {
     Card(
-        shape = RoundedCornerShape(10.dp),
+
         modifier = Modifier
             .fillMaxWidth()
-            .height(80.dp)
+            .height(80.dp),
+        shape = RoundedCornerShape(10.dp),
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.secondary)
     ) {
-        AsyncImage(
-            model = ImageRequest
-                .Builder(LocalContext.current)
-                .data(R.drawable.ic_image)
-                .crossfade(true)
-                .placeholder(R.drawable.ic_image)
-                .build(),
-            contentDescription = "IMAGE",
-            fallback = painterResource(R.drawable.ic_image),
-            error = painterResource(R.drawable.ic_image),
-            contentScale = ContentScale.FillWidth,
+        Column(
             modifier = Modifier
-                .clickable {}
-        )
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            AsyncImage(
+                model = ImageRequest
+                    .Builder(LocalContext.current)
+                    .data(R.drawable.ic_image)
+                    .crossfade(true)
+                    .placeholder(R.drawable.ic_image)
+                    .build(),
+                contentDescription = "IMAGE",
+                fallback = painterResource(R.drawable.ic_image),
+                error = painterResource(R.drawable.ic_image),
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .clickable {}
+            )
+        }
     }
 }
 
@@ -314,7 +330,7 @@ fun ShoppingListCategoryItem() {
                     .padding(start = 16.dp)
             )
 
-            Column {
+            Column() {
                 for (i in 1..3) {
                     ShoppingListIngredientItem()
 
@@ -337,7 +353,8 @@ fun ShoppingListIngredientItem() {
         verticalAlignment = Alignment.CenterVertically
     ) {
         Box(
-            modifier = Modifier.size(40.dp)
+            modifier = Modifier.size(40.dp),
+            contentAlignment = Alignment.Center
         ) {
             AsyncImage(
                 model = ImageRequest
@@ -381,6 +398,227 @@ fun ShoppingListIngredientItem() {
         Checkbox(
             checked = false,
             onCheckedChange = {}
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun AddRecipeContent() {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Add recipe") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back button"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = "Add recipe button"
+                        )
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(horizontal = 16.dp)
+        ) {
+            Text(
+                text = "Title",
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier
+                    .padding(bottom = 4.dp)
+            )
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 24.dp)
+            )
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 24.dp)
+                    .clickable {}
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(30.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.PhotoCamera,
+                        contentDescription = "Add photo button"
+                    )
+
+                    Text(
+                        text = "Add photo",
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 12.sp,
+                        modifier = Modifier
+                            .padding(top = 8.dp)
+                    )
+                }
+            }
+
+            Text(
+                text = "Description",
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier
+                    .padding(bottom = 4.dp)
+            )
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 24.dp)
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 4.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Ingredients",
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                Text(
+                    text = "Reorder",
+                    fontWeight = FontWeight.Light,
+                    fontSize = 12.sp
+                )
+            }
+
+            Text(
+                text = "Tap to edit, swipe to delete",
+                fontWeight = FontWeight.Light,
+                fontSize = 12.sp,
+                modifier = Modifier
+                    .padding(bottom = 20.dp)
+            )
+
+            Column(
+                modifier = Modifier
+                    .padding(bottom = 20.dp)
+            ) {
+                for (i in 1..4) {
+                    RecipeIngredientItem()
+
+                    if (i != 4) {
+                        HorizontalDivider()
+                    }
+                }
+            }
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 24.dp)
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Servings",
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                TextButton(
+                    onClick = {}
+                ) {
+                    Text(text = "Text Button")
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Prep time",
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                TextButton(
+                    onClick = {}
+                ) {
+                    Text(text = "Set time")
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RecipeIngredientItem() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Max)
+            .clickable {},
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier.size(40.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            AsyncImage(
+                model = ImageRequest
+                    .Builder(LocalContext.current)
+                    .data(R.drawable.ic_image)
+                    .crossfade(true)
+                    .placeholder(R.drawable.ic_image)
+                    .build(),
+                contentDescription = "IMAGE",
+                fallback = painterResource(R.drawable.ic_image),
+                error = painterResource(R.drawable.ic_image),
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .clickable {}
+            )
+        }
+
+        Text(
+            text = "Ingredient",
+            modifier = Modifier
+                .padding(start = 8.dp)
         )
     }
 }
